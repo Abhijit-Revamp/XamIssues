203f1d94c0cd26a5637e99c1368f37a851cb2ed7
Abhijit-Revamp <abhijit.deshpande@revamp.in>
2018-11-11 17:30:21 +0530

    Initial check-in of module XamIssues

---
+++ XamIssues.macOS/AppDelegate.cs
@@ -0,0 +1,51 @@
+﻿using AppKit;
+using CoreGraphics;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace XamIssues.macOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        readonly NSWindow _window;
+
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var frameSz = NSScreen.MainScreen.Frame;
+
+            var rect = new CGRect(500, 1000, 500, 500);
+            _window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            _window.SetFrame(frameSz, true, true);
+            _window.BackgroundColor = NSColor.Black;
+
+            _window.Title = "Xamarin Issues";
+        }
+        
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+
+        public override NSWindow MainWindow
+        {
+            get { return _window; }
+        }
+
+        [Export("applicationShouldTerminateAfterLastWindowClosed:")]
+        public override bool ApplicationShouldTerminateAfterLastWindowClosed(NSApplication sender)
+        {
+            return true;
+        }
+    }
+}
---
+++ XamIssues/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamIssues
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        //https://github.com/Abhijit-Revamp/XamIssues.git
+        public Color BkClr { get; set; } = Color.Red;
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            BkClr = (BkClr == Color.Red) ? Color.Black : Color.Red;
+            NotifyPropertyChanged("BkClr");
+        }
+
+        public new event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
